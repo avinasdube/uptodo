99408c99dc3e7afac92752173b46264cd96817fc
avinasdube <avidubey712@gamil.com>
2023-11-29 13:58:30 +0530

    [functionality] : enabled all functionalities

--- src/components/SingleTask/SingleTask.jsx
+++ src/components/SingleTask/SingleTask.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import './SingleTask.scss';
+import { checkTodo, removeTodo } from '../../reducers/todoReducer';
+import { useDispatch } from 'react-redux';
+
+import edit from '../../assets/edit.png';
+import remove from '../../assets/delete.png';
+import { Link } from 'react-router-dom';
 
 const SingleTask = ({ tasks }) => {
+
+    const dispatch = useDispatch();
+
+    const handleCheck = (id) => {
+        dispatch(checkTodo(id))
+    }
     return (
         <>
-            {
+            {tasks.length === 0 ?
+                <div style={{ textAlign: 'center' }}>No tasks found</div> :
                 tasks.map((task) => (
                     <div className="taskContainer" key={task.id}>
-                        <input type='checkbox'></input>
-                        <div className='details'>
-                            <div className="title">{task.taskName}<div className="priority">{task.priority}</div></div>
-                            <div className="descrip">{task.taskDescrip}</div>
+                        <div className="left">
+                            <input type='checkbox' onChange={() => handleCheck(task.id)} checked={task.checked}></input>
+                            <div className='details'>
+                                <div className="title">{task.taskName}
+                                    {task.priority === 'Low' ?
+                                        <div className="priority" style={{ backgroundColor: 'lightgreen', color: 'green' }}>{task.priority}</div> :
+                                        task.priority === 'Medium' ?
+                                            <div className="priority" style={{ backgroundColor: 'lightyellow', color: 'orange' }}>{task.priority}</div> :
+                                            <div className="priority" style={{ backgroundColor: 'pink', color: 'red' }}>{task.priority}</div>}</div>
+                                <div className="descrip">{task.taskDescrip}</div>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <Link className='link' to={`/edit/:${task.id}`}><img src={edit} alt=''></img></Link>
+                            <img src={remove} alt='' onClick={()=>dispatch(removeTodo(task.id))}></img>
                         </div>
                     </div>
                 ))
--- src/pages/TaskList/TaskList.jsx
+++ src/pages/TaskList/TaskList.jsx
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TaskList.scss';
 import SingleTask from '../../components/SingleTask/SingleTask';
+import { useDispatch, useSelector } from 'react-redux';
+
+import filter from '../../assets/filter.png';
+import { setActiveFilter } from '../../reducers/filterReducer';
 
 const TaskList = () => {
 
-  const tasks = [
-    {
-      id: 1,
-      taskName: "Write project documentation",
-      taskDescrip: "Write the documentation for ongoing task 'UpTodo' before Thursday.",
-      priority: "High",
-    },
-    {
-      id: 2,
-      taskName: "Visit Akshardham",
-      taskDescrip: "Go to visit Akshardham mandir in Delhi",
-      priority: "Low",
-    },
-    {
-      id: 3,
-      taskName: "Prepare for interview",
-      taskDescrip: "Start preparing for the upcoming interview at AulaCube",
-      priority: "Medium",
-    }
-  ]
+  const [popup, setPopup] = useState(false);
+
+  const handlePopup = () => {
+    popup === true ? setPopup(false) : setPopup(true)
+  }
+
+  const dispatch = useDispatch();
+
+  // GETTING TASKLIST FROM REDUX STATE REDUCER
+  const tasks = useSelector(state => state.todos.todos);
+  const activeFilter = useSelector(state => state.filtars.activeFilter);
+
+  // FILTERING THE TASKLIST IF ANY FILTERS ARE SELECTED ELSE RETURN ALL TASKS
+  const filteredTasks = activeFilter === "All Tasks" ? tasks : tasks.filter((task) => task.priority === activeFilter)
 
   return (
     <div className="tasklistContainer">
-      <SingleTask tasks={tasks} />
+      <div className="filters">
+        <div className={`activeFilter ${popup === true ? 'active' : ''}`} onClick={handlePopup}>
+          <img src={filter} alt=''></img><span>{activeFilter}</span>
+        </div>
+        <div className={`filterOptions ${popup === true ? 'active' : ''}`}>
+          <span onClick={() => dispatch(setActiveFilter('Low'))}>Low</span>
+          <span onClick={() => dispatch(setActiveFilter('Medium'))}>Medium</span>
+          <span onClick={() => dispatch(setActiveFilter('High'))}>High</span>
+          <span onClick={() => dispatch(setActiveFilter('All Tasks'))}>All Tasks</span>
+        </div>
+      </div>
+      <SingleTask tasks={filteredTasks} />
     </div>
   )
 }
